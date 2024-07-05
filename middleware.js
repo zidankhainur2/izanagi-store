20486d29f0d2342a4433c8a54488a13a44c2d354
Ahmad Fauzidan <zidankhainur2@gmail.com>
2024-07-05 12:33:20 +0700

    fixing bug

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  apiKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
