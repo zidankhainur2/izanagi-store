8647385b2142e8ed68a50e93bef36ee99d0b4860
Ahmad Fauzidan <zidankhainur2@gmail.com>
2024-07-23 17:11:35 +0700

    fix bug 1

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware({
-  apiKey: "pk_test_d2lyZWQta2l0ZS04OS5jbGVyay5hY2NvdW50cy5kZXYk",
-  secretKey: "sk_test_RiM7GWf9dbvhfoQAKZohhh9Mv7dnX2dDovjj8q4Nsf",
+  apiKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
 });
 
 export const config = {
